8a70a668917e5550463fb09406ba141c1d4b538c
jkr2255 <jkr2255@users.noreply.github.com>
2016-06-04 14:18:55 +0900

    implement code

--- lib/function_module.rb
+++ lib/function_module.rb
@@ -1,5 +1,19 @@
 require "function_module/version"
 
-module FunctionModule
-  # Your code goes here...
+class Module # :nodoc:
+
+  def module_functions
+    original_private_methods = private_instance_methods
+    original_private_methods -= Module.new.private_instance_methods
+    original_singular_methods = public_methods - Module.public_methods
+    original_private_methods & original_singular_methods
+  end
+
+  private
+
+  def include_module_functions(mod)
+    include mod
+    extend mod
+    singleton_class.__send__ :public, *mod.module_functions
+  end
 end
--- spec/function_module_spec.rb
+++ spec/function_module_spec.rb
@@ -4,8 +4,53 @@ describe FunctionModule do
   it 'has a version number' do
     expect(FunctionModule::VERSION).not_to be nil
   end
+end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+describe 'Module#module_functions' do
+  it 'is a public method' do
+    expect(Module).to be_method_defined(:module_functions)
+  end
+
+  it 'returns empty array with empty Module' do
+    expect(Module.new.module_functions).to eq []
+  end
+
+  it 'returns properly with methods defined by module_function' do
+    module Test1 # :nodoc:
+      def foo; end
+      module_function :foo
+    end
+    expect(Test1.module_functions).to eq [:foo]
+  end
+
+  it 'doesn\'t count singular/private only methods' do
+    module Test2 # :nodoc:
+      def self.bar; end
+      def baz; end
+      private :baz
+    end
+    expect(Test2.module_functions).to eq []
+  end
+
+  it 'doesn\'t count singular & public methods' do
+    module Test3 # :nodoc:
+      def self.hoge; end
+      def hoge; end
+    end
+    expect(Test3.module_functions).to eq []
+  end
+end
+
+describe 'Module#include_module_functions' do
+  it 'is a private method' do
+    expect(Module).to be_private_method_defined(:include_module_functions)
+  end
+
+  it 'introduces other Module\'s module functions' do
+    module Test5
+      include_module_functions Math
+    end
+    expect(Test5).to respond_to(:sin)
+    expect(Test5).to be_private_method_defined(:sin)
   end
 end
